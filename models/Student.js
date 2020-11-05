442f68f40e3183c7bc7ef4e7979c2fc9fd356ce5
Ben Paget <ben.paget@live.fr>
2020-11-05 12:26:51 +0000

    Mongoose intro course

---
+++ models/Student.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const studentSchema = new Schema({
+    first_name: {type: String, min: 2, max: 50, required: true},
+    last_name: {type: String, min: 2, max: 50, required: true},
+    last_updated: {type: Date, default: Date.now}
+})
+
+const Student = mongoose.model('Student', studentSchema)
+
+module.exports = Student
+
+// Mongoose Data Types:
+// https://mongoosejs.com/docs/schematypes.html
+// String
+// Number
+// Date
+// Buffer
+// Boolean
+// ObjectId
+// Array
+// ...
